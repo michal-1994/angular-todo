e36407fe2d09414b1b2881102350e47d747351de
Michal <michalgrzegorczyk04@gmail.com>
2022-06-11 15:05:24 +0200

    Add item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,10 +19,16 @@ export class AppComponent {
   }
 
   get itemCount(): number {
-    return this.list.items.filter(item => !item.complete).length;
+    return this.list.items.length;
   }
 
   get items(): readonly TodoItem[] {
-    return this.list.items;
+    return this.list.items.filter(item => !item.complete);
+  }
+
+  addItem(newItem: string) {
+    if (newItem != "") {
+      this.list.addItem(newItem);
+    }
   }
 }
