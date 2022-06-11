3af4aa1e47d753e0f2fbb32ba2271d42c741c4ab
Michal <michalgrzegorczyk04@gmail.com>
2022-06-11 14:28:46 +0200

    Add header of list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { TodoItem } from './todoItem';
+import { TodoList } from './todoList';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo';
+  private list = new TodoList("Adam", [
+    new TodoItem("Iść pobiegać", false),
+    new TodoItem("Kupić kwiaty"),
+    new TodoItem("Odebrać bilet")
+  ]);
+
+  get username(): string {
+    return this.list.user;
+  }
+
+
+  get itemCount(): number {
+    return this.list.items.filter(item => !item.complete).length;
+  }
 }
